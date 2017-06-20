4ae9c4b164a9ed829ae0c468ff50626e3573b9c7
Rafael Vidal <Rafael Vidal>
2017-06-20 00:37:36 -0300

    Data Structure

---
+++ SimuladorMIPSSuperEscalar/src/DataStructure/ROB.java
@@ -0,0 +1,18 @@
+package DataStructure;
+
+import java.util.ArrayList;
+
+public class ROB {
+	
+	ArrayList robList = new ArrayList<ROBNode>(10);
+	
+	public boolean isFull(){
+		if (robList.size()>=10){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+}
